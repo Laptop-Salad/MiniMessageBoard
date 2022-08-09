43fac8ec40ee0312dc5705da87699b15f9886377
Amanda <squaretacos456@gmail.com>
2022-08-09 13:46:49 +0100

    add files

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Morning Bartholomew",
+    user: "PhiladelphiaGarlicAndHerbs",
+    added: new Date()
+  },
+  {
+    text: "Morning, Lovely weather isn't it?",
+    user: "Bartholomew",
+    added: new Date()
+  }
+]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.messageText, user: req.body.username, added: new Date()});
+  res.redirect('/');
+});
+
+module.exports = router;
